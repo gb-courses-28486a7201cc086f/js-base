470b3b0780be31685f91a5cbc91851427b9d18e7
Vitaly <vintik-shpuntik@yandex.ru>
2020-11-26 13:28:35 +0300

    refactor after review

---
+++ hw-2/arifmetic.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function sum(a, b) {
+    return Number(a) + Number(b);
+}
+
+function difference(a, b) {
+    return Number(a) - Number(b);
+}
+
+function composition(a, b) {
+    return Number(a) * Number(b);
+}
+
+function division(a, b) {
+    return Number(a) / Number(b);
+}
